247560dd4d1e0f1bfe0a2879886ebbd98e335d13
Laffy <huuhoag1412@gmail.com>
2022-10-12 20:08:45 +0700

    #5 tạo class tác giả

---
+++ src/Model/Sach.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class Sach  {
+
+    private final int id;
+    private String tenSach;
+    private int tongSoTrang;
+    private String ngonNgu;
+    private String tacGia;
+    private String nhaXuatBan;
+    private short namXuatBan;
+    private String tinhTrang;
+    private String gioiThieu;
+
+    public Sach(int id) {
+        this.id = id;
+    }
+
+
+
+}
